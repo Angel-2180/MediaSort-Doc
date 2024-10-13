b043d054dceb0249cc85e66d7770bcebf04a97a7
Swann <swann.rouanet@gmail.com>
2024-10-13 17:43:46 +0200

    feat: switch to theme.config.tsx and update theme config

---
+++ theme.config.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>MediaSort 🦦</span>,
+  project: {
+    link: "https://github.com/Angel-2180/MediaSort",
+  },
+  footer: {
+    content: "Made by 💖 by Angel2180 & Hezaerd",
+  },
+};
+
+export default config;
